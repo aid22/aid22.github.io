09c6a04de48c91800d9c36152b6986e97e7bddc5
Aiden <118379307+awa03@users.noreply.github.com>
2024-05-27 14:07:02 -0400

    New Resume

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-function showPopup() {
-    document.getElementById("popup").style.display = "block";
-  }
-  
-  function closePopup() {
-    document.getElementById("popup").style.display = "none";
-  }
-  
+const navLinks = document.querySelectorAll('nav a');
+
+function getCurrentSection() {
+    const sections = document.querySelectorAll('section');
+    let currentSection = null;
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (window.scrollY >= sectionTop - sectionHeight / 2) {
+            currentSection = section;
+        }
+    });
+
+    return currentSection;
+}
+
+function updateNavigation() {
+    const currentSection = getCurrentSection();
+    if (currentSection) {
+        const currentSectionId = currentSection.getAttribute('id');
+        // Remove the 'selected' class from all links
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+        });
+        // Add the 'selected' class to the corresponding link
+        const correspondingNavLink = document.querySelector(`nav a[href="#${currentSectionId}"]`);
+        if (correspondingNavLink) {
+            correspondingNavLink.classList.add('active');
+        }
+    }
+}
+
+window.addEventListener('DOMContentLoaded', updateNavigation);
+window.addEventListener('scroll', updateNavigation);
+
+
+function runEducation() {
+    var x = document.getElementById("education-output");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function blinkCursor() {
+    var cursor = document.getElementById("cursor");
+    cursor.style.visibility = (cursor.style.visibility == "visible") ? "hidden" : "visible";
+}
+setInterval(blinkCursor, 1000);
+
