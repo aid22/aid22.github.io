0a5f3769104a01aadf08b8500f9f5217e9ea411b
Erik Dubois <118379307+awa03@users.noreply.github.com>
2025-01-18 14:50:55 -0500

    Remove

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "./aid22.github.io",
-  plugins: [react()],
-})
